dc8b757b7f2ee7d45e618be9f74605f3c87f0fa6
haripriya <haripriya@qburst.com>
2021-12-30 00:26:06 +0530

    Table responsiveness

--- src/app/app-table/app-table.component.ts
+++ src/app/app-table/app-table.component.ts
@@ -5,33 +5,56 @@ import { IColumnConfig } from 'src/interfaces/tableConfig';
   selector: 'app-table',
   templateUrl: './app-table.component.html',
   styleUrls: ['./app-table.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 
 export class AppTableComponent implements OnChanges {
+  @Input('tableConfig') tableConfig: IColumnConfig[] = [];
+  @Input('tableData') tableData: any[] = [];
 
-  @Input("tableConfig") tableConfig: IColumnConfig[] = [];
-  @Input("tableData") tableData: any[] = [];
+  public tableColumns: {
+    [index: string]: any;
+    leftColumns: IColumnConfig[];
+    rightColumns: IColumnConfig[];
+    scrollableColumns: IColumnConfig[];
+  } = {
+    leftColumns: [],
+    rightColumns: [],
+    scrollableColumns: [],
+  };
 
-  public scrollableColumns: IColumnConfig[] = [];
-  public leftPinnedColumns: IColumnConfig[] = [];
-  public rightPinnedColumns: IColumnConfig[] = [];
   public noResults: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnChanges(changes: SimpleChanges): void {
     // Display table if data is present
     if (this.tableConfig.length && this.tableData.length) {
       this.noResults = false;
-      this.scrollableColumns = this.getPinnedColumns('none');
-      this.leftPinnedColumns = this.getPinnedColumns('left');
-      this.rightPinnedColumns = this.getPinnedColumns('right');
-    }
-    else this.noResults = true;
+      this.tableColumns = {
+        leftColumns: this.getPinnedColumns('left'),
+        rightColumns: this.getPinnedColumns('right'),
+        scrollableColumns: this.getPinnedColumns('none'),
+      };
+    } else this.noResults = true;
   }
 
-  public getPinnedColumns(direction: "left" | "right" | "none") {
-    return this.tableConfig.filter(col => col.pinColumn === direction);
+  public getPinnedColumns(direction: 'left' | 'right' | 'none') {
+    return this.tableConfig.filter((col) => col.pinColumn === direction);
+  }
+
+  // Style columns
+  public columnStyle(columnWidth: any) {
+    return columnWidth ? {'min-width': columnWidth + 'px'} : { 'width': this.getAutoColumnWidth('none'), 'min-width': '200px'}
+  }
+
+  // Function to apply equal width to each column with no predefined width
+  private getAutoColumnWidth(direction: 'left' | 'right' | 'none'): string {
+    return `${
+      100 /
+      this.tableColumns[
+        direction === 'none' ? 'scrollableColumns' : `${direction}Columns`
+      ].filter((col: IColumnConfig) => !col?.columnWidth).length
+    }%`;
   }
 }
